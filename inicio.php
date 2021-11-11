1010d3495753de239171d273c5adc5b3eb1a9671
reppc <84331772+reppc@users.noreply.github.com>
2021-11-11 10:23:51 -0600

    Añadir inicio (no completado)

---
+++ inicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>CEFAL</title>
+	<link rel="stylesheet" type="text/css" href="../Boostrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/css-inicio.css">
+</head>
+<body>
+
+	
+	<div class="row" style="background-color: #3F2D3B;">
+			<div class="col 6 tipoL"><a href="">CEFAL</a></div>
+			<div class="col 6 tipoL"><a href=""><img src="img/inicio.png" style="width: 30pt;">Inicio</a></div>
+			<div class="col 6 tipoL"><a href="">Metas</a></div>
+			<div class="col 1 tipoL"><a href="">Egresados</a></div>
+			<div class="col 1 tipoL"><a href="">Becas</a></div>
+			<div class="col 1 tipoL"><a href="">Servicio social</a></div>
+			<div class="col 1 tipoL"><a href="">Convocatorias</a></div>
+			<div class="col 1 tipoL navbar-toggler"><a href="" class="btn btn primary border" style="color:lightgray">Registrate</a></div>
+			
+	</div>
+<div class="row" style="background-color:skyblue; width: 1042pt;">si</div>
+
+</body>
+</html>
