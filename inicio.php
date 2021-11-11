64f654d977e7f268cd2c9f0e7dc67a6db34f0b75
reppc <84331772+reppc@users.noreply.github.com>
2021-11-11 10:40:13 -0600

    img-1

--- inicio.php
+++ inicio.php
@@ -3,15 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	<title>CEFAL</title>
-	<link rel="stylesheet" type="text/css" href="../Boostrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/css-inicio.css">
+	<link rel="stylesheet" type="text/css" href="Bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="Css/Inicio.css">
 </head>
 <body>
 
 	
 	<div class="row" style="background-color: #3F2D3B;">
 			<div class="col 6 tipoL"><a href="">CEFAL</a></div>
-			<div class="col 6 tipoL"><a href=""><img src="img/inicio.png" style="width: 30pt;">Inicio</a></div>
+			<div class="col 6 tipoL"><a href=""><img src="img/inicio.png" style="width: 15pt;">&nbsp;Inicio</a></div>
 			<div class="col 6 tipoL"><a href="">Metas</a></div>
 			<div class="col 1 tipoL"><a href="">Egresados</a></div>
 			<div class="col 1 tipoL"><a href="">Becas</a></div>
