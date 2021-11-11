65a3fb7c7a9f15f1e42460f3963a6226b38a9587
reppc <84331772+reppc@users.noreply.github.com>
2021-11-11 15:26:19 -0600

    Navbar terminado

--- inicio.php
+++ inicio.php
@@ -10,14 +10,14 @@
 
 	
 	<div class="row" style="background-color: #3F2D3B;">
-			<div class="col 6 tipoL"><a href="">CEFAL</a></div>
+			<div class="col 6 tipoL"><a href="" style="font-size: 40px; letter-spacing: 2px">CEFAL</a></div>
 			<div class="col 6 tipoL"><a href=""><img src="img/inicio2.png" style="width: 15pt;">&nbsp;Inicio</a></div>
-			<div class="col 6 tipoL"><a href=""><img src="img/metas.png"style="width: 15pt;"> Metas</a></div>
-			<div class="col 1 tipoL"><a href=""><img src="img/egre.png"style="width: 30pt;">Egresados</a></div>
-			<div class="col 1 tipoL"><a href="">Becas</a></div>
-			<div class="col 1 tipoL"><a href="">Servicio social</a></div>
-			<div class="col 1 tipoL"><a href="">Convocatorias</a></div>
-			<div class="col 1 tipoL navbar-toggler"><a href="" class="btn btn primary border" style="color:lightgray">Registrate</a></div>
+			<div class="col 6 tipoL"><a href=""><img src="img/sis.png"style="width: 15pt;">Sistemas</a></div>
+			<div class="col 1 tipoL"><a href=""><img src="img/egre.png"style="width: 18pt;">Administracion</a></div>
+			<div class="col 1 tipoL"><a href=""><img src="img/becas.png"style="width: 30pt;">Becas</a></div>
+			<div class="col 1 tipoL"><a href=""><img src="img/serv.png"style="width: 18pt;">Servicio social</a></div>
+			<div class="col 1 tipoL"><a href=""><img src="img/conv.png"style="width: 15pt;">Convocatorias</a></div>
+			<div class="col 1 tipoL navbar-toggler"><a href="" class="reg btn btn primary border" style="color:lightgray" id="reg">Registrate</a></div>
 			
 	</div>
 <div class="row" style="background-color:skyblue; width: 101.6%;">si</div>
