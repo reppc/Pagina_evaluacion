76e3d17d4876dd3a94af34a77a33c803089ab239
reppc <84331772+reppc@users.noreply.github.com>
2021-11-11 13:55:29 -0600

    imgs

--- inicio.php
+++ inicio.php
@@ -11,16 +11,16 @@
 	
 	<div class="row" style="background-color: #3F2D3B;">
 			<div class="col 6 tipoL"><a href="">CEFAL</a></div>
-			<div class="col 6 tipoL"><a href=""><img src="img/inicio.png" style="width: 15pt;">&nbsp;Inicio</a></div>
-			<div class="col 6 tipoL"><a href="">Metas</a></div>
-			<div class="col 1 tipoL"><a href="">Egresados</a></div>
+			<div class="col 6 tipoL"><a href=""><img src="img/inicio2.png" style="width: 15pt;">&nbsp;Inicio</a></div>
+			<div class="col 6 tipoL"><a href=""><img src="img/metas.png"style="width: 15pt;"> Metas</a></div>
+			<div class="col 1 tipoL"><a href=""><img src="img/egre.png"style="width: 30pt;">Egresados</a></div>
 			<div class="col 1 tipoL"><a href="">Becas</a></div>
 			<div class="col 1 tipoL"><a href="">Servicio social</a></div>
 			<div class="col 1 tipoL"><a href="">Convocatorias</a></div>
 			<div class="col 1 tipoL navbar-toggler"><a href="" class="btn btn primary border" style="color:lightgray">Registrate</a></div>
 			
 	</div>
-<div class="row" style="background-color:skyblue; width: 1042pt;">si</div>
+<div class="row" style="background-color:skyblue; width: 101.6%;">si</div>
 
 </body>
 </html>
