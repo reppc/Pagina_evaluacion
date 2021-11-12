336dc4f53e3f00eeae4265a9f3d5c46120a80d27
echodegrasa <74998947+echodegrasa@users.noreply.github.com>
2021-11-11 21:48:44 -0600

    creasion de carpeta items y creasion del nav bar

--- inicio.php
+++ inicio.php
@@ -7,20 +7,12 @@
 	<link rel="stylesheet" type="text/css" href="Css/Inicio.css">
 </head>
 <body>
-
-	
-	<div class="row" style="background-color: #3F2D3B;">
-			<div class="col 6 tipoL"><a href="" style="font-size: 40px; letter-spacing: 2px">CEFAL</a></div>
-			<div class="col 6 tipoL"><a href=""><img src="img/inicio2.png" style="width: 15pt;">&nbsp;Inicio</a></div>
-			<div class="col 6 tipoL"><a href=""><img src="img/sis.png"style="width: 15pt;">Sistemas</a></div>
-			<div class="col 1 tipoL"><a href=""><img src="img/egre.png"style="width: 18pt;">Administracion</a></div>
-			<div class="col 1 tipoL"><a href=""><img src="img/becas.png"style="width: 30pt;">Becas</a></div>
-			<div class="col 1 tipoL"><a href=""><img src="img/serv.png"style="width: 18pt;">Servicio social</a></div>
-			<div class="col 1 tipoL"><a href=""><img src="img/conv.png"style="width: 15pt;">Convocatorias</a></div>
-			<div class="col 1 tipoL navbar-toggler"><a href="" class="reg btn btn primary border" style="color:lightgray" id="reg">Registrate</a></div>
-			
-	</div>
-<div class="row" style="background-color:skyblue; width: 101.6%;">si</div>
+ <?php
+ require_once("items/navbar.php");
+ ?>
+<div class="row" style="background-color:skyblue; width: 101.6%;">
+<p>hola</p>
+</div>
 
 </body>
 </html>
---
+++ items/navbar.php
@@ -0,0 +1,16 @@
+<?php
+echo"
+
+<div class='row' style='background-color: #3F2D3B;'>
+			<div class='col 6 tipoL'><a href='' style='font-size: 40px; letter-spacing: 2px'>CEFAL</a></div>
+			<div class='col 6 tipoL'><a href=''><img src='img/inicio2.png' style='width: 15pt;'>&nbsp;Inicio</a></div>
+			<div class='col 6 tipoL'><a href=''><img src='img/sis.png'style='width: 15pt;'>Sistemas</a></div>
+			<div class='col 1 tipoL'><a href=''><img src='img/egre.png'style='width: 18pt;'>Administracion</a></div>
+			<div class='col 1 tipoL'><a href=''><img src='img/becas.png'style='width: 30pt;'>Becas</a></div>
+			<div class='col 1 tipoL'><a href=''><img src='img/serv.png'style='width: 18pt;'>Servicio social</a></div>
+			<div class='col 1 tipoL'><a href=''><img src='img/conv.png'style='width: 15pt;'>Convocatorias</a></div>
+			<div class='col 1 tipoL navbar-toggler'><a href='' class='reg btn btn primary border' style='color:lightgray' id='reg'>Registrate</a></div>
+			
+	</div>
+";
+?>
